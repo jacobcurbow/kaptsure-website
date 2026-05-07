3efe628096d8630909d050a91eca48713e095a71
Jacob Curbow <jacob@Jacob-iMac.local>
2026-05-07 13:09:23 -0500

    Fix Kaptsure contact email

--- lib/contact.ts
+++ lib/contact.ts
@@ -1,4 +1,4 @@
 export const phoneDisplay = "719-205-7777";
 export const phoneHref = "tel:+17192057777";
-export const emailAddress = "jacob@katpsure.com";
+export const emailAddress = "jacob@kaptsure.com";
 export const quoteMailto = `mailto:${emailAddress}?subject=Kaptsure%20Quote%20Request`;
