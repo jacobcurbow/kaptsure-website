1aab1d296cc37abb5d10e757b0c92ac6f5b7c452
Jacob Curbow <jacob@Jacob-iMac.local>
2026-05-08 10:17:30 -0500

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+
+const googleAnalyticsId = "G-7WQTTPZDX6";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://kaptsure.com"),
@@ -27,6 +30,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleAnalyticsId}');
+          `}
+        </Script>
         <Header />
         {children}
         <Footer />
